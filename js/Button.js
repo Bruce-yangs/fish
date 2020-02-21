14c05781138d9150be5c4e4abdd70dfacde34062
yangkun <yangkun@leying365.com>
2020-02-21 20:24:54 +0800

    game-fish

---
+++ js/Button.js
@@ -0,0 +1,46 @@
+class Button extends Spirit {
+  constructor(data, data_down) {
+
+    super({
+      img: data.img,
+      w: data.frame.w,
+      h: data.frame.h,
+      sx: data.frame.x,
+      sy: data.frame.y
+    });
+    this.data = data;
+    this.data_down = data_down;
+  }
+
+  //检测是否点击
+  _check(x,y) {
+    if (this.x-this.w/2 <= x && x <= this.x + this.w/2
+      && this.y - this.h/2 <= y && y <= this.y + this.h/2
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  checkDown(x,y) {
+    if (this._check(x,y)) {
+      this.img = this.data_down.img;
+      this.sx = this.data_down.frame.x;
+      this.sy = this.data_down.frame.y;
+      this.w = this.data_down.frame.w;
+      this.h = this.data_down.frame.h;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  checkUp(x,y) {
+    if (this._check(x,y)) {
+      this.img = this.data.img;
+      this.sx = this.data.frame.x;
+      this.sy = this.data.frame.y;
+      this.w = this.data.frame.w;
+      this.h = this.data.frame.h;
+    }
+  }
+}
---
+++ js/common.js
@@ -0,0 +1,8 @@
+function rndInt(n,m) {
+  return Math.floor(Math.random()*(m-n)+n);
+}
+
+//随机角度
+function rnd(n,m) {
+  return Math.random()*(m-n)+n;
+}
